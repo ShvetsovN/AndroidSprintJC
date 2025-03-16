f60c07ae63beeed5bf7822d83471db37aef2164e
= <=>
2025-03-16 15:01:22 +0500

    feat/RCA-04 custom typo

--- app/src/main/java/com/example/androidsprintjc/MainActivity.kt
+++ app/src/main/java/com/example/androidsprintjc/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidsprintjc.ui.theme.AndroidSprintJCTheme
+import com.example.androidsprintjc.ui.theme.recipesAppTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +20,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidSprintJCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Text(
+                            text = "БУРГЕР С ГРИБАМИ 1 (titleLarge)",
+                            style = recipesAppTypography.titleLarge
+                        )
+                        Text(
+                            text = "ИНГРЕДИЕНТЫ 2 (titleMedium)",
+                            style = recipesAppTypography.titleMedium
+                        )
+                        Text(
+                            text = "Порции 3 (titleSmall)",
+                            style = recipesAppTypography.titleSmall
+                        )
+                        Text(
+                            text = "Вы еще не добавили ни... 1 (bodyLarge)",
+                            style = recipesAppTypography.bodyLarge
+                        )
+                        Text(
+                            text = "В то время как котлеты..., 2 (bodyMedium)",
+                            style = recipesAppTypography.bodyMedium
+                        )
+                        Text(
+                            text = "КАТЕГОРИИ 1 (labelLarge)",
+                            style = recipesAppTypography.labelLarge
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidSprintJCTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/androidsprintjc/ui/theme/FontFamily.kt
@@ -0,0 +1,12 @@
+package com.example.androidsprintjc.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.androidsprintjc.R
+
+val MontserratAlternates = FontFamily(
+    Font(R.font.montserrat_alternates_regular),
+    Font(R.font.montserrat_alternates_medium),
+    Font(R.font.montserrat_alternates_semibold),
+    Font(R.font.montserrat_alternates_bold),
+)
