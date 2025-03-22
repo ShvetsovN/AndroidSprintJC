8fd1e1c3f2a25ba6e1f0e61c9d09b50eececfa8f
= <=>
2025-03-22 17:36:28 +0500

    feat: Add colors in Color.kt

--- app/src/main/java/com/example/androidsprintjc/ui/theme/Color.kt
+++ app/src/main/java/com/example/androidsprintjc/ui/theme/Color.kt
@@ -9,3 +9,22 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Colors for Light Theme
+val PrimaryColor = Color(0xFF5F3678)
+val AccentColor = Color(0xFFE13E3E)
+val AccentBlue = Color(0xFF525DC0)
+val BackgroundColor = Color(0xFFF4F4FF)
+val SurfaceColor = Color(0xFFFFFFFF)
+val SurfaceVariantColor = Color(0xFF303030)
+val TextPrimaryColor = Color(0xFF000000)
+val TextSecondaryColorval = Color(0xFF666666)
+
+//Colors for Dark Theme
+val PrimaryColorDark = Color(0xFF9A9EFF)
+val AccentColorDark = Color(0xFFE57373)
+val AccentBlueDark = Color(0xFF64B5F6)
+val BackgroundColorDark = Color(0xFF121212)
+val SurfaceColorDark = Color(0xFF1E1E1E)
+val TextPrimaryColorDark = Color(0xFFE6E6E6)
+val TextSecondaryColorvalDark = Color(0xFFB0B0B0)
